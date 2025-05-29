c354d84fef1ff3c79c772f14455b3f957101686d
ikromzzoda <git-ikromi2004@mail.ru>
2025-05-29 18:22:26 +0500

    added change_info function in user/views.py

--- sejong_backend/users/urls.py
+++ sejong_backend/users/urls.py
@@ -3,9 +3,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path('groups/', views.get_user_groups, name='get_user_groups'),
     path('login/', views.login_view, name='login_view'),
-    path('change_username/', views.change_username, name='change_username'),
-    path('change_password/', views.change_password, name='change_password'),
+    path('groups/', views.get_user_groups, name='get_user_groups'),
     path('profile/', views.get_profile_info, name='get_profile_info'),
+    path('change_info/', views.change_info, name='change_info'),
+    # path('change_username/', views.change_username, name='change_username'),
+    # path('change_password/', views.change_password, name='change_password'),
+    # path('number/', views.change_number, name='change_number'),
 ]
