8f5c2bf4c359db1a56c550083a5be3a30d45ac62
ikromzzoda <git-ikromi2004@mail.ru>
2025-05-29 18:22:26 +0500

    added change_info function in user/views.py

--- sejong_backend/users/views.py
+++ sejong_backend/users/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import User, Groups
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 import json
 from rest_framework.authtoken.models import Token
 
@@ -70,50 +69,6 @@ def get_profile_info(request):
         })            
             
 
-    
-@csrf_exempt
-def change_username(request):
-    if request.method == "POST":
-        token = check_token(request)
-        if token:
-            try:
-                data = json.loads(request.body.decode("utf-8"))
-                new_username = data.get("new_username")
-                if not new_username:
-                    return JsonResponse({"error": "Username is required"}, status=400)
-                
-                if User.objects.filter(username = new_username).exists():
-                    return JsonResponse({"error": "Username already taken"})
-                
-                token.username = new_username
-                token.save()
-                return JsonResponse({"message": "Username updated successfully", "new_username": new_username})
-
-            except Exception as e:
-                return JsonResponse({"ERROR": str(e)})
-            
-    return JsonResponse({"error": "Only POST requests are allowed"})
-
-@csrf_exempt
-def change_password(request):
-    if request.method == "POST":
-        token = check_token(request)
-        if token:
-            try:
-                data = json.loads(request.body.decode("utf-8"))
-                new_password = data.get("new_password")
-
-                if not new_password:
-                    return JsonResponse({"error": "Password is required"}, status=400)
-                
-                token.set_password(new_password)
-                token.save()
-                return JsonResponse({"message": "Password updated successfully", "new_password": new_password})
-
-            except Exception as e:
-                return JsonResponse({"ERROR": str(e)})
-        
-    return JsonResponse({"error": "Only POST requests are allowed"})
 
 @csrf_exempt
 def change_avatar(request):
@@ -133,26 +88,108 @@ def change_avatar(request):
             except Exception as e:
                 return JsonResponse({"ERROR": str(e)})
         
-    return JsonResponse({"error": "Only POST requests are allowed"})
+    return JsonResponse({"error": "Only POST requests are allowed"})                
 
 
 @csrf_exempt
-def change_number(request):
+def change_info(request):
     if request.method == "POST":
         token = check_token(request)
         if token:
             try:
-                data = json.load(request.body.decode("UTF-8"))
-                new_number = data.get("new_number")
-
-                if not new_number:
-                    return JsonResponse({"message": "PhoneNumber is required"}, status=400)
+                data = json.loads(request.body.decode("UTF-8"))
+                username = data.get("username")
+                password = data.get("password")
+                phone_number = data.get("phone_number")
+                email = data.get("email")
+                avatar = data.get("avatar")
                 
-                token.phone_number = new_number
-                token.save()
+                if username:
+                    token.username = username
+                
+                if password:
+                    token.set_password(password)
+                
+                if phone_number:
+                    token.phone_number = phone_number
+                    print(phone_number)
+                
+                if email:
+                    token.email = email
+                
+                # if avatar:
+                #     change_avatar(avatar)
 
+                token.save()
+                return JsonResponse({ "message": f"Success, data: {token}" })
+            
             except Exception as e:
                 return JsonResponse({"ERROR": str(e)})
 
-    return JsonResponse({"error": "Only POST requests are allowed"})
+
+
+# @csrf_exempt
+# def change_username(request):
+#     if request.method == "POST":
+#         token = check_token(request)
+#         if token:
+#             try:
+#                 data = json.loads(request.body.decode("utf-8"))
+#                 new_username = data.get("new_username")
+#                 if not new_username:
+#                     return JsonResponse({"error": "Username is required"}, status=400)
                 
+#                 if User.objects.filter(username = new_username).exists():
+#                     return JsonResponse({"error": "Username already taken"})
+                
+#                 token.username = new_username
+#                 token.save()
+#                 return JsonResponse({"message": "Username updated successfully", "new_username": new_username})
+
+#             except Exception as e:
+#                 return JsonResponse({"ERROR": str(e)})
+            
+#     return JsonResponse({"error": "Only POST requests are allowed"})
+
+# @csrf_exempt
+# def change_password(request):
+#     if request.method == "POST":
+#         token = check_token(request)
+#         if token:
+#             try:
+#                 data = json.loads(request.body.decode("utf-8"))
+#                 new_password = data.get("new_password")
+
+#                 if not new_password:
+#                     return JsonResponse({"error": "Password is required"}, status=400)
+                
+#                 token.set_password(new_password)
+#                 token.save()
+#                 return JsonResponse({"message": "Password updated successfully", "new_password": new_password})
+
+#             except Exception as e:
+#                 return JsonResponse({"ERROR": str(e)})
+        
+#     return JsonResponse({"error": "Only POST requests are allowed"})
+
+
+# @csrf_exempt
+# def change_number(request):
+#     if request.method == "POST":
+#         token = check_token(request)
+#         if token:
+#             try:
+#                 data = json.loads(request.body.decode("UTF-8"))
+#                 new_number = data.get("new_number")
+
+#                 if not new_number:
+#                     return JsonResponse({"message": "PhoneNumber is required"}, status=400)
+                
+#                 token.phone_number = new_number
+#                 token.save()
+#                 return JsonResponse({"message": "Data updated successfully"})
+
+#             except Exception as e:
+#                 return JsonResponse({"ERROR": str(e)})
+
+#     return JsonResponse({"error": "Only POST requests are allowed."})
