0fc46d2762427c246146f9f17788402966aaf057
ikromzzoda <git-ikromi2004@mail.ru>
2025-05-30 18:44:15 +0500

    changed something in user/views.py and urls.py

--- sejong_backend/users/urls.py
+++ sejong_backend/users/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login_view'),
-    path('groups/', views.get_user_groups, name='get_user_groups'),
     path('profile/', views.get_profile_info, name='get_profile_info'),
+    # path('avatar/', views.change_avatar, name='change_avatar'),
     path('change_info/', views.change_info, name='change_info'),
+    # path('groups/', views.get_user_groups, name='get_user_groups'),
     # path('change_username/', views.change_username, name='change_username'),
     # path('change_password/', views.change_password, name='change_password'),
     # path('number/', views.change_number, name='change_number'),
