d5fff81a32749dededee6f72cf27731fb0fdfaa8
ikromzzoda <git-ikromi2004@mail.ru>
2025-06-02 15:46:55 +0500

    changing in elibrary/views.py and info/views.py

--- sejong_backend/info/views.py
+++ sejong_backend/info/views.py
@@ -1,54 +1,53 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Schedule, Announcement
 from rest_framework.authtoken.models import Token
 
-def check_token(request):
-    auth_token = request.headers.get("token")
-    if not auth_token:
-        return JsonResponse({"error": "Token not provided"}, status=401)
+# def check_token(request):
+#     auth_token = request.headers.get("token")
+#     if not auth_token:
+#         return JsonResponse({"error": "Token not provided"}, status=401)
 
-    try:
-        token = Token.objects.get(key=auth_token)
-        user = token.user
-    except Token.DoesNotExist:
-        return JsonResponse({"error": "Invalid token"}, status=401)
+#     try:
+#         token = Token.objects.get(key=auth_token)
+#         user = token.user
+#     except Token.DoesNotExist:
+#         return JsonResponse({"error": "Invalid token"}, status=401)
 
-    return user
+#     return user
 
 def get_schedules(request):
     if request.method == "GET":
-        token = check_token(request)
-        if token:
-            schedules = Schedule.objects.all()
-            data = []
+        # token = check_token(request)
+        # if token:
+        schedules = Schedule.objects.all()
+        data = []
 
-            for schedule in schedules:
-                data.append({
-                    'group': schedule.group.first().name if schedule.group.exists() else None,
-                    'teacher': schedule.teacher,
-                    'book': schedule.book,
-                    "time": schedule.time if schedule.time else [],
-                })
-            return JsonResponse(data, safe=False)
+        for schedule in schedules:
+            data.append({
+                'group': schedule.group.first().name if schedule.group.exists() else None,
+                'teacher': schedule.teacher,
+                'book': schedule.book,
+                "time": schedule.time if schedule.time else [],
+            })
+        return JsonResponse(data, safe=False)
 
 def get_all_announcements(request):
     if request.method == "GET":
-        token = check_token(request)
-        if token:
-            # Assuming you have a model named AnnouncementImage with a field 'image'
-            announcements = Announcement.objects.all()
-            data = []
+        # token = check_token(request)
+        # if token:
+        # Assuming you have a model named AnnouncementImage with a field 'image'
+        announcements = Announcement.objects.all()
+        data = []
 
-            for announcement in announcements:
-                data.append({
-                    "title": announcement.title,
-                    "content": announcement.content,
-                    "images": announcement.images,
-                    "time_posted": announcement.time_posted.strftime("%Y-%m-%d %H:%M:%S"),
-                    "author": announcement.author,
-                    "is_active": announcement.is_active,
-                    "custom_id": announcement.custom_id,
-                })
-                
-            return JsonResponse(data, safe=False)
+        for announcement in announcements:
+            data.append({
+                "title": announcement.title,
+                "content": announcement.content,
+                "images": announcement.images,
+                "time_posted": announcement.time_posted.strftime("%Y-%m-%d %H:%M:%S"),
+                "author": announcement.author,
+                "is_active": announcement.is_active,
+                "custom_id": announcement.custom_id,
+            })
+            
+        return JsonResponse(data, safe=False)
