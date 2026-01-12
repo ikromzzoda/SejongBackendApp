ccc4d700ea55b9b2b0bc0ac79113f978ecab3e4f
ikromzzoda <ikromi2004@gmail.com>
2026-01-12 10:57:32 +0500

    tst commit

--- sejong_backend/info/urls.py
+++ sejong_backend/info/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('schedules/', views.get_schedules, name='get_schedules'),
     path('announcements/', views.get_all_announcements, name='get_all_announcements'),
-    path('notice/', views.get_notices, name='get_notices'),
+    path('notice/', views.get_notices, name='get_notices')
 ]
